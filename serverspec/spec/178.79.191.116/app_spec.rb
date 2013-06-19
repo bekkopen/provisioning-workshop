e46112d32c0a5d4552c7da7ea834363784f6ee2a
Stein Inge Morisbak <stein.inge.morisbak@BEKK.no>
2013-06-19 12:42:58 +0200

    More specs

---
+++ serverspec/spec/178.79.191.116/app_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+#describe file('/etc/init.d/devops') do
+#  it { should be_file }
+#  it { should contain "PID_FILE=/home/devops/devops.pid" }
+#  it { should contain "APP_HOME=/home/devops" }
+#  it { should contain "APP_JAR=devops.jar" }
+#  it { should contain "LOGS=/home/devops/logs" }
+#end
--- serverspec/spec/178.79.191.116/httpd_spec.rb
+++ serverspec/spec/178.79.191.116/httpd_spec.rb
@@ -15,8 +15,18 @@ describe port(80) do
   it { should be_listening }
 end
 
+describe port(1234) do
+  it { should be_listening }
+end
+
 describe file('/etc/nginx/conf.d/devops.conf') do
   it { should be_file }
+  it { should contain "server {" }
+  it { should contain "listen 80;" }
+  it { should contain "server_name _;" }
+  it { should contain "access_log /var/log/nginx/devops-access.log;" }
+  it { should contain "location / {" }
   it { should contain "proxy_pass http://localhost:1234;" }
   it { should contain "proxy_redirect off;" }
 end
+
