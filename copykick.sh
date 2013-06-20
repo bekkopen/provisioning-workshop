39d1cc06007f22907a5ad2d0916e57d1e2d90ea0
Espen Herseth Halvorsen <espen.halvorsen@bekk.no>
2013-06-20 18:02:01 +0200

    Fikser på deployscript + flere brukere

--- copykick.sh
+++ copykick.sh
@@ -1,3 +1,7 @@
 #/bin/bash
 
 scp -r puppet/* root@178.79.160.213:/etc/puppetlabs/puppet/
+
+
+ssh root@176.58.109.121 "cat /var/run/pe-puppet/agent.pid | xargs kill -s SIGUSR1"
+ssh root@178.79.160.213 "cat /var/run/pe-puppet/agent.pid | xargs kill -s SIGUSR1"
--- puppet/manifests/httpserver.pp
+++ puppet/manifests/httpserver.pp
@@ -8,4 +8,20 @@ class httpserver {
 			shell => '/bin/bash',
 	}
 
+	user {
+		'espenhh' : 
+			ensure => present,
+			home => '/home/espenhh',
+			password => sha1('devops123'),
+			shell => '/bin/bash',
+	}
+
+	user {
+		'arp' : 
+			ensure => present,
+			home => '/home/arp',
+			password => sha1('devops123'),
+			shell => '/bin/bash',
+	}
+
 }
