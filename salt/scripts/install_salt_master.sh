7f36c98f083659a913e642843ae0cc21aa567b13
Stein Inge Morisbak <stein.inge.morisbak@BEKK.no>
2013-08-22 15:16:14 +0200

    salt key

--- salt/scripts/install_salt_master.sh
+++ salt/scripts/install_salt_master.sh
@@ -5,3 +5,5 @@ sudo chkconfig salt-master on
 sudo cp /vagrant/files/master /etc/salt/master
 sudo service salt-master start
 
+sudo /usr/bin/salt-key -A && echo Keys: && sudo /usr/bin/salt-key -L && echo Ping && sudo /usr/bin/salt '*' test.ping
+
--- salt/scripts/install_salt_minion.sh
+++ salt/scripts/install_salt_minion.sh
@@ -5,4 +5,3 @@ sudo chkconfig salt-minion on
 sudo cp /vagrant/files/minion /etc/salt/minion
 sudo service salt-minion start
 
-sudo /usr/bin/salt-key -A && echo Keys: && sudo /usr/bin/salt-key -L && echo Ping && sudo /usr/bin/salt '*' test.ping
