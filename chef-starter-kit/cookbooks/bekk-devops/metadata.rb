d60a3110ab198ffa123b7f46bcee291c0b83795c
Fredrik Valdmanis <fredrik.valdmanis@bekk.no>
2013-08-26 11:03:31 +0200

    Lagt til cookbook som setter opp riktig bruker

---
+++ chef-starter-kit/cookbooks/bekk-devops/metadata.rb
@@ -0,0 +1,7 @@
+name             'bekk-devops'
+maintainer       'YOUR_COMPANY_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures bekk-devops'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
---
+++ chef-starter-kit/cookbooks/bekk-devops/recipes/default.rb
@@ -0,0 +1,29 @@
+#
+# Cookbook Name:: bekk-devops
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+user "devops" do
+  supports :manage_home => true 
+  system true
+  home "/home/devops"
+  shell "/bin/bash"
+  password "$1$cgoRmMec$Ge8EgZkjjwvKfOkcOAn7a1"
+  action :create
+end
+
+#cookbook_file "/tmp/devops" do
+#  source "devops"
+#  mode 0755
+#end
+
+#execute "run-devops-init-script" do
+#  command "sh /tmp/devops"
+#end
+
+
+
