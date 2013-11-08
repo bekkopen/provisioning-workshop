76e018ac69558db8e06a84357fd7a5590163fe63
Dag Henning Liodden Sørbø <dag.henning.liodden.sorbo@bekk.no>
2013-11-08 11:55:09 +0100

    nginx conf

--- fagdag-puppet/manifests/nginx.pp
+++ fagdag-puppet/manifests/nginx.pp
@@ -1,3 +1,18 @@
-package { 'nginx' :
-    ensure => 'latest'
+class nginx {
+
+	package { 'nginx' :
+    		ensure => 'latest'
+	}
+
+	file { 'nginx.conf' :
+		path => '/etc/nginx/conf.d/devops.conf',
+		ensure => file,
+		source => '/etc/puppy/files/devops.conf'
+	}
+
+	service { 'nginx' :
+		require => Package['nginx'],
+		require => File['nginx.conf'],
+		ensure  => running
+	}
 }
